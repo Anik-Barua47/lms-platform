cc87e7a336241a249ca5e1b288a0184b8fb7d691
Anik-Barua47 <anik.b.xponent@gmail.com>
2025-01-28 16:42:10 +0600

    create top navbar

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,30 @@
+import { Menu } from "lucide-react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Sidebar } from "./sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <SheetHeader>
+          <SheetTitle></SheetTitle>
+          <SheetDescription></SheetDescription>
+        </SheetHeader>
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
