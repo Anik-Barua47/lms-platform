765b20959cc8eaca9d1434ad44f7d13f69b4fd6f
Anik-Barua47 <anik.b.xponent@gmail.com>
2025-02-01 13:35:23 +0600

    course title updated

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = await auth();
+    const { courseId } = await params; // Ensure that params are awaited before using
+
+    const values = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.update({
+      where: { id: courseId, userId },
+      data: { ...values },
+    });
+    console.log(values);
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.error("COURSE_ID", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
