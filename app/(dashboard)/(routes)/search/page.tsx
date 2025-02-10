9ed68508b8af6df65ff92bae05d96cbc6a4fcbd0
Anik-Barua47 <anik.b.xponent@gmail.com>
2025-02-10 10:34:33 +0600

    final

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
@@ -10,13 +11,11 @@ interface SearchPageProps {
   searchParams: {
     title: string;
     categoryId: string;
-  }
-};
+  };
+}
 
 const SearchPage = async ({ searchParams }: SearchPageProps) => {
-
   const { userId } = await auth();
-
   if (!userId) {
     return redirect("/");
   }
@@ -35,7 +34,10 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
   return (
     <>
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
-        <SearchInput />
+        {/* Wrap SearchInput in a Suspense boundary */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchInput />
+        </Suspense>
       </div>
       <div className="p-6 space-y-4">
         <Categories items={categories} />
